90930f830a1ec9208ccaef6bff09d41d18e8941d
lynn josse <lynn.josse@monsanto.com>
2020-03-19 20:06:12 -0500

    it works

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+
+  this.fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+  .then(function(response){
+    response.json()
+    .then(function(response){
+      console.table(response)
+      const container = document.getElementById("container");
+      container.innerHTML = astronautParser(response)
+    })
+  })
+})
+
+function astronautParser(astronauts) {
+  let astronautHTML = ''
+  for (astronaut of astronauts) {
+    astronautHTML += 
+    `<div class="astronaut">
+      <div class="bio">
+        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+        <ul>
+            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+            <li>Active: ${astronaut.active}</li>
+            <li>Skills: ${astronaut.skills.join(', ')}</li>
+        </ul>
+      </div>
+      <img class="avatar" src=${astronaut.picture}>
+  </div>`
+
+  }
+  return astronautHTML
+}
